4d5417e316c13ed2e2131990290c1a9f3a33195c
Sara Zambrano <sarahzambrano29@gmail.com>
2025-10-05 18:48:03 -0300

    Proyecto Final + Zambrano - Definitivo

--- src/Firebase.js
+++ src/Firebase.js
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyB97HGgdABZzToT89DZ2uiuCnMMpc8hl6s',
-  authDomain: 'novalux-feb8f.firebaseapp.com',
-  projectId: 'novalux-feb8f',
-  storageBucket: 'novalux-feb8f.firebasestorage.app',
-  messagingSenderId: '520354963325',
-  appId: '1:520354963325:web:116e4da2734a680990b614',
-  measurementId: 'G-VD7GY75NX8',
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
